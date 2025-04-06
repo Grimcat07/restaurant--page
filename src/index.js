28a437b7496826308052a66cc0d637cb11e73080
Grimcat07 <166890725+Grimcat07@users.noreply.github.com>
2025-04-06 19:34:31 +0530

    completed menu page without dish images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./styles.css"
 import {homepage} from "./homepage.js"
-
+import {menu} from "./menu.js"
 
 
 const homepage_button=document.querySelector("#homepage");
 homepage_button.addEventListener("click",()=>homepage())
-
+const menu_button=document.querySelector("#menupage")
+menu_button.addEventListener("click",()=>menu())
 homepage();
